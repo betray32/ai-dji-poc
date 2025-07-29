11d73d8b92c317e98517cb57d45cd66deb1af5b6
Camilo E. Contreras <contreras.camilo32@gmail.com>
2025-07-29 14:25:39 -0400

    initial push

---
+++ java/DJI-IA/src/main/java/com/milo/DJI_IA/DjiIaApplication.java
@@ -0,0 +1,13 @@
+package com.milo.DJI_IA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjiIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjiIaApplication.class, args);
+	}
+
+}
